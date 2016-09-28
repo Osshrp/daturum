5809ab6f1b9b13267256b618c1616fd69a57a60b
Osshrp <osshrp@gmail.com>
2016-09-28 16:07:12 +0300

    add question spec, controller, model

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,6 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+    render @questions
+  end
+end
--- spec/controllers/questions_spec.rb
+++ spec/controllers/questions_spec.rb
@@ -4,14 +4,14 @@ describe QuestionsController, type: :request do
 
   describe "GET #index" do
     # let!(:posts) { FactoryGirl.create_list(:post, 2) }
-    before { get api_posts_url }
+    before { get questions_url }
 
     it "has a 200 status code" do
       expect(response.status).to eq(200)
     end
 
-    it "populates an array of all posts" do
-      expect(response.body).to eq(posts.to_json)
-    end
+    # it "populates an array of all questions" do
+    #   expect(response.body).to eq(posts)
+    # end
   end
 end
